a38fd7f75ab01420e200175625176397f7540ba9
sohee <soheekim980.naver.com>
2024-07-23 16:33:47 +0900

    css file path change

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const baseurl = "https://kim-sohee.github.io/portfolio/";
+
+app.use(express.static(path.join(baseurl, 'public')));
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get('/test/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "test.html"));
+    res.sendFile(path.join(baseurl, "index.html"));
 });
 
 app.listen(port, ()=>{
